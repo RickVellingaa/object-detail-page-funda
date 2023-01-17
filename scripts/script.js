8476a218470b6099fe0a6b9d23352f80f2742f46
RickVellingaa <rickvellinga2408@gmail.com>
2023-01-17 20:37:11 +0100

    Dark mode knop toegevoegd

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,17 @@
-console.log("Hallo")
+let lightMode = document.querySelector('#moon')
+let darkMode = document.querySelector('#sun')
+
+lightMode.addEventListener('click', hideElementMoon);
+darkMode.addEventListener('click', hideElementSun);
+
+function hideElementMoon() {
+    lightMode.classList.toggle('hidden');
+    darkMode.classList.toggle('hidden');
+    event.preventDefault();
+}
+
+function hideElementSun() {
+    lightMode.classList.toggle('hidden');
+    darkMode.classList.toggle('hidden');
+    event.preventDefault();
+}
