682206cc0fc59aa788268e6d481d8603f04180cc
RickVellingaa <rickvellinga2408@gmail.com>
2023-01-18 23:53:42 +0100

    Hart interactie

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,28 @@
 let lightMode = document.querySelector('#moon')
 let darkMode = document.querySelector('#sun')
+let heartAnim = document.querySelector('.heart-animation')
 
+heartAnim.addEventListener('click', heartAnimation);
 lightMode.addEventListener('click', hideElementMoon);
 darkMode.addEventListener('click', hideElementSun);
 
-function hideElementMoon() {
+function hideElementMoon(event) {
     lightMode.classList.toggle('hidden');
     darkMode.classList.toggle('hidden');
     event.preventDefault();
 }
 
-function hideElementSun() {
+function hideElementSun(event) {
     lightMode.classList.toggle('hidden');
     darkMode.classList.toggle('hidden');
+    event.preventDefault();
+}
+
+const calculateHeight = () => {
+    return '100%';
+}
+
+function heartAnimation(event) {
+    heartAnim.style.setProperty('--height', calculateHeight());
     event.preventDefault();
 }
