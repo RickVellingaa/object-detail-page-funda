bd3af27d9ee78fdc665e5575c7c930b9c6c9324d
RickVellingaa <rickvellinga2408@gmail.com>
2023-01-19 22:29:58 +0100

    Tweaks + Nieuwe functie
    Heb tweaks aan nav bar toegepast en context geplaatst met een read more functie.

--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,9 @@ let lightMode = document.querySelector('#moon')
 let darkMode = document.querySelector('#sun')
 let heartAnim = document.querySelector('.heart-animation')
 
-heartAnim.addEventListener('click', heartAnimation);
 lightMode.addEventListener('click', hideElementMoon);
 darkMode.addEventListener('click', hideElementSun);
+heartAnim.addEventListener('click', heartAnimation);
 
 function hideElementMoon(event) {
     lightMode.classList.toggle('hidden');
@@ -26,3 +26,19 @@ function heartAnimation(event) {
     heartAnim.style.setProperty('--height', calculateHeight());
     event.preventDefault();
 }
+
+function ReadMore() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("readBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Lees meer"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Lees minder"; 
+      moreText.style.display = "inline";
+    }
+  }
