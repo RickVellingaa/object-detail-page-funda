ccf06806475d3f84ae5dc7999e26dd7f20cc79ef
RickVellingaa <rickvellinga2408@gmail.com>
2023-01-23 16:11:59 +0100

    Modal img functie, meer informatie van huis toegevoegd

--- scripts/script.js
+++ scripts/script.js
@@ -1,44 +1,72 @@
-let lightMode = document.querySelector('#moon')
-let darkMode = document.querySelector('#sun')
 let heartAnim = document.querySelector('.heart-animation')
+let heartColor = document.querySelector('.fa-heart')
 
-lightMode.addEventListener('click', hideElementMoon);
-darkMode.addEventListener('click', hideElementSun);
 heartAnim.addEventListener('click', heartAnimation);
 
-function hideElementMoon(event) {
-    lightMode.classList.toggle('hidden');
-    darkMode.classList.toggle('hidden');
-    event.preventDefault();
-}
-
-function hideElementSun(event) {
-    lightMode.classList.toggle('hidden');
-    darkMode.classList.toggle('hidden');
-    event.preventDefault();
-}
-
 const calculateHeight = () => {
-    return '100%';
+  return '1.2em';
 }
 
 function heartAnimation(event) {
-    heartAnim.style.setProperty('--height', calculateHeight());
-    event.preventDefault();
+  heartAnim.style.setProperty('--height', calculateHeight());
+  heartColor.style.color = "red";
+  event.preventDefault();
 }
 
 function ReadMore() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("readBtn");
-  
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Lees meer"; 
-      moreText.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Lees minder"; 
-      moreText.style.display = "inline";
-    }
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("readBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Lees meer";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Lees minder";
+    moreText.style.display = "inline";
   }
+}
+
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var img1 = document.getElementById("myImg1");
+var img2 = document.getElementById("myImg2");
+
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function (event) {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+  event.preventDefault();
+}
+
+img1.onclick = function (event) {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+  event.preventDefault();
+}
+
+img2.onclick = function (event) {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+  event.preventDefault();
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+modal.onclick = function () {
+  modal.style.display = "none";
+}
